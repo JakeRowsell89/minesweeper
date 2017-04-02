2632d32085e092aeb14b9daa4a991c28b714084f
Jake Rowsell <jake@qubit.com>
2017-04-02 19:06:24 +0100

    Less added succesfully

--- app/index.js
+++ app/index.js
@@ -2,4 +2,6 @@ import Game from './components/Game'
 import React from 'react'
 import {render} from 'react-dom'
 
+require('less-loader!./css/style.less')
+
 render(<Game />, document.getElementById('app'))
